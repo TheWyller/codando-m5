39c07f42392c3b28eba64c2b8011ee33cdf64681
TheWyller <wyller_gt@hotmail.com>
2022-11-01 17:11:18 -0300

    FEATURE: Adicionando view de listagem de todos os posts de uma mesma categoria

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('posts/', views.PostView.as_view()),
     path('posts/<str:post_id>/', views.PostDetailView.as_view()),
+    path(
+        'posts/categories/<str:category_id>/',
+        views.PostOnCategoryView.as_view()
+        ),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from rest_framework import generics
 
+from django.shortcuts import get_object_or_404
+
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-
 from posts.permissions import ListUpdateDeletePermission
 
 from .models import Post
+from categories.models import Category
+
 from .serializers import PostSerializer
+from categories.serializers import CategorySerializer
+
+
+def get_object_by_id(model, **kwargs):
+    object = get_object_or_404(model, **kwargs)
+
+    return object
 
 
 class PostView(generics.ListCreateAPIView):
@@ -26,3 +36,15 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
+
+class PostOnCategoryView(generics.ListAPIView):
+
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    lookup_url_kwarg = "category_id"
+
+    def get_queryset(self):
+        category_id = self.kwargs["category_id"]
+        category = get_object_by_id(Category, id=category_id)
+        return category.posts
