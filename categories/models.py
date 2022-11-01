1797b3a359c8e658c7f49b8e84f0fb799eb02bf3
marco <marcoxd01@hotmail.com>
2022-11-01 15:00:41 -0300

    FEATURE: criando model/serialzier de category e atualizando requirements.txt

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=255, unique=True)
