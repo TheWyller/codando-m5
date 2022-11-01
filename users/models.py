d2ed3f129d43fd952e389b86098e30c2aa2036d8
TheWyller <wyller_gt@hotmail.com>
2022-11-01 09:42:51 -0300

    FEATURE: Adicionando model e serializer de Usuários

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    REQUIRED_FIELDS = ["first_name", "last_name", "is_seller"]
