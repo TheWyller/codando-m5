69483885779abb9778bf5117b46a687ed5b9afbb
TheWyller <wyller_gt@hotmail.com>
2022-11-04 14:55:45 -0300

    FIX: Resolvendo rotas com problemas com permissões erradas

---
+++ commets/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+
+class ListUpdateDeletePermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+
+        if request.method == 'GET':
+            return True
+
+        if request.user.is_superuser:
+            return True
+        elif obj == request.user:
+            return True
+        else:
+            return False
--- commets/urls.py
+++ commets/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('posts/<str:post_id>/comments/', views.CommentView.as_view()),
-    path('posts/<str:post_id>/comments/<str:comment_id>', views.CommentDetailView.as_view()),
+    path('posts/<str:post_id>/comments/<str:comment_id>/', views.CommentDetailView.as_view()),
 ]
